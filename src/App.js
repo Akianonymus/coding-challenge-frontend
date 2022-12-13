4b2ac2322459a30a48f46a414ed122ac4f9f5da6
Akianonymus <anonymus.aki@gmail.com>
2022-12-14 00:48:55 +0530

    Initial Push
    Implemented Forms
    
    Show progress
    
    Use google maps to show map
    
    use tailwind css
    
    dark mode support

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { useMemo, useState } from "react";
+import Form from "./Form";
 
 function App() {
+  const [darkMode, setDarkMode] = useState(null);
+
+  // update local storage when theme changes
+  useMemo(() => {
+    if (darkMode !== null)
+      if (darkMode)
+        // Whenever the user explicitly chooses dark mode
+        localStorage.theme = "dark";
+      // Whenever the user explicitly chooses light mode
+      else localStorage.theme = "light";
+  }, [darkMode]);
+
+  // if theme var found in localStorage then use it
+  // otherwise check prefers-color-scheme
+  useMemo(() => {
+    if (
+      localStorage.theme === "dark" ||
+      ((localStorage?.theme !== "light" || !("theme" in localStorage)) &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches)
+    ) {
+      setDarkMode(true);
+      localStorage.theme = "dark";
+    } else {
+      setDarkMode(false);
+      localStorage.theme = "light";
+    }
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={darkMode ? "dark" : ""}>
+      <div className="bg-white min-h-screen text-sky-900 dark:text-sky-100 dark:bg-gray-900">
+        <div className="max-w-7xl mx-auto">
+          <div className="flex">
+            <button
+              className="text-center mx-auto rounded-md px-2 py-2 bg-gray-200 dark:bg-gray-800 hover:bg-gray-300 dark:hover:bg-gray-700 sm:w-[66%] lg:w-[40%] w-[90%] my-3 tracking-wide font-bold text-2xl"
+              onClick={() => window.location.reload()}
+            >
+              ADmyBrand India
+            </button>
+          </div>
+          <Form />
+        </div>
+      </div>
     </div>
   );
 }
